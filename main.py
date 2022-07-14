b7f94ed471c5c72c11ebe263f6ae75d96288c1e6
che10mahar <109257908+che10mahar@users.noreply.github.com>
2022-07-14 08:03:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+while True:
+   time.sleep(1)
+   pyautogui.typewrite("sagar bisht gadhe ki list ")
+   pyautogui.press('enter')sagar bisht gadhe ki list
+agar bisht gadhe ki list
+sagar bisht gadhe ki list
